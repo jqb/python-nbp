3527c78c1698481fe0424dd1f27b0ee88165f545
Kuba Janoszek <kuba@laptop.(none)>
2011-11-09 18:23:57 +0100

    setup.py file added.

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+
+setup(
+    name='nbp',
+    version='1.0',
+    description='Polish National Bank currency rate downloader.',
+    author='Kuba Janoszek',
+    author_email='kuba.janoszek@gmail.com',
+    url='https://github.com/jqb/python-nbp',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 1 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    )
