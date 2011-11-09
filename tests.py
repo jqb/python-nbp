e3548b60dcbb48a55fcd25fe77b0b7a3904043df
Kuba Janoszek <kuba@laptop.(none)>
2011-11-09 20:57:32 +0100

    test for new 'url' param updated.

--- tests.py
+++ tests.py
@@ -9,9 +9,12 @@ def test_downloads_exchange_rate_properly():
         'search_date': '2010-07-11',
         'table_no': u'132/A/NBP/2010',
         'pub_date': u'2010-07-09',
+        'url': 'http://rss.nbp.pl/kursy/xml2/2010/a/10a132.xml',
         'currency': {
             'name': u'euro',
             'rate': 4.0732999999999997,
-            'code': u'EUR'}}
+            'code': u'EUR',
+            },
+        }
     currency_data = nbp.download_exchange_rate(nbp.date(2010, 7, 11), 'EUR')
     assert_equals(expexted, currency_data)
